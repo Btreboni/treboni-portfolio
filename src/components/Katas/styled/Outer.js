d2c0efce15f4c760c48b882dd7bae9d581c869b6
Btreboni <btreboni@gmail.com>
2020-02-04 12:21:20 -0500

    fixing buttons

--- src/components/Katas/styled/Outer.js
+++ src/components/Katas/styled/Outer.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components'
 
 const Outer = styled.div`
+    margin: 8% auto 0px auto;
 
+    button{
+        padding: 8px;
+        color: black;
+        border-radius: 12px;
+        background-color: white;
+        border: 2px solid black;
+        transition-duration: 0.4s;
+    }
+
+    button:hover {
+        color: white;
+        font-weight: bold;
+        background-color: black;
+    }
 `
 
 export default Outer
