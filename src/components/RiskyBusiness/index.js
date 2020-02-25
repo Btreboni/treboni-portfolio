be9572c17f4f4edfb6f1c202f2b9a486d0bb5ecb
Btreboni <btreboni@gmail.com>
2020-02-25 10:52:17 -0500

    rick roll added

--- src/components/RiskyBusiness/index.js
+++ src/components/RiskyBusiness/index.js
@@ -3,18 +3,64 @@ import React, { PureComponent } from 'react'
 //styled
 import Outer from './styled/Outer'
 
+//components
+import FancyButton from '../FancyButton'
+
 export default class RiskyBusiness extends PureComponent{
     constructor(props){
         super(props)
         this.state = {
-
+            showLink: false,
+            showTitle: true
         }
     }
 
+    handleClick = () =>{
+        this.setState({showTitle: true ? false : true});
+    }
+
+    handleLink = () =>{
+        this.setState({showLink: true, showTitle: false});
+    }
+
+    sendIt = () =>{
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
+
     render(){
+
+        const {
+            showLink,
+            showTitle
+        } = this.state
         return(
             <Outer>
-                <h1>TESTING</h1>
+                {
+                    showTitle
+                    ?
+                    <div>
+                        <h1>Risky Business</h1>
+                        <h3>Full disclosure, clicking this button is risky.</h3>
+                        <FancyButton handleClick={this.handleClick}>
+                            You probably should not click me
+                        </FancyButton>
+                    </div>
+                    :
+                    showLink
+                    ?
+                    <div className={"last-chance"}> 
+                        <h3>Last chance to turn back!</h3>
+                        <FancyButton handleClick={this.sendIt}>
+                            Abandon All Hope Ye Who Enter Here
+                        </FancyButton>
+                    </div>
+                    :
+                    <div className={"second-title"}>
+                        <FancyButton handleClick={this.handleLink}>
+                            You're sure you want to do this?
+                        </FancyButton>
+                    </div>
+                }
             </Outer>
         )
     }
--- src/components/RiskyBusiness/styled/Outer.js
+++ src/components/RiskyBusiness/styled/Outer.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components'
 
 const Outer = styled.div`
+    font-family: 'Overpass Mono', monospace , sans-serif;
+    margin: 8% 0 2% 0;  
 
+    h3{
+        margin: 1em;
+        margin-bottom: 4em;
+    }
+
+    .second-title{
+        margin-top: 21%;
+    }
+
+    .last-chance{
+        margin-top: 10%;
+    }
 `
 
 export default Outer;
